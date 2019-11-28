bdc551c2797d659dd5acb54e1965003d25a81aee
Stefanie Söhnitz <stefanie.soehnitz@hs-duesseldorf.de>
2019-11-28 14:42:11 +0100

    add html_generator.py

---
+++ html_generator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 13:58:11 2019
+
+@author: ruesoe
+"""
+
+authors = ["Darwin", "Lovelace", "Hawkin", "Noether"]
+
+output_fh = open("list.html", "w")
+    
+scaffold_start = """<!DOCTYPE html>
+<html>
+<head>
+<title>Titel dieser wunderbaren Seite</title>
+</head>
+<body>
+
+<h1>Eine grandiose Überschrift</h1>
+
+<p>Ein nicht weniger toller Abschnitt.</p>
+
+<ul>
+"""
+
+output_fh.write(scaffold_start)
+scaffold_middle = ""
+
+for author in authors:
+    scaffold_middle = scaffold_middle + "<li> " + author + "</li>\n"
+
+output_fh.write(scaffold_middle)
+scaffold_end ="""
+</ul>
+</body>
+
+</html>"""
+output_fh.write(scaffold_end)
+
+output_fh.close()
